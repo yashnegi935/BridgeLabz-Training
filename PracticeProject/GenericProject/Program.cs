ab35b58094f5c683e73ffc5a600a22bff927aa1a
yash <yashnegi935@gmail.com>
2026-08-16 23:49:59 +0530

    Mini Project on NUnit

--- PracticeProject/GenericProject/Program.cs
+++ PracticeProject/GenericProject/Program.cs
@@ -5,4 +5,11 @@ user.id=10123;
 user.email="yashnegi@gmail.com";
 user.Name = "Yash";
 
+User<string> user2 = new User<string>();
+
+user2.id = "102";
+user2.Name = "Rahul";
+user2.email = "rahul@gmail.com";
+
+Check.display(user2);
 Check.display(user);
